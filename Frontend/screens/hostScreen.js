92f0cd9b8193eabbfc08f09f370ac247e12e0608
Tanguille <Tanguille.grootaert@student.hogent.be>
2022-11-30 19:01:20 +0100

    Added screens

---
+++ Frontend/screens/hostScreen.js
@@ -0,0 +1,3 @@
+import Navigation from "../src/components/Navbar";
+
+<Navigation />
--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navigation = () => {
     return (
         <Navbar bg="light" expand="lg">
             <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand href="#home">Studenten Opvolging</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
